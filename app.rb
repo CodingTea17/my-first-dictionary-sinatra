c6b448a94948502b90863875f6b3e87fd44d859b
Dawson M <dawson.mortenson@gmail.com>
2017-09-15 13:37:20 -0700

    add the ability to add multiple definitions to a word and it comes with checking for empty definitions

--- app.rb
+++ app.rb
@@ -10,16 +10,38 @@ end
 
 post('/new_word') do
   word = Word.new(params["new_word"])
-  word.save()
-  # Super awesome redirect that will reroute the user to a GET route after the new word is saved to the dictionary. Prevents error when reloading the page after making the POST.
-  redirect '/dictionary'
+  if word.save()
+    # Super awesome redirect that will reroute the user to a GET route after the new word is saved to the dictionary.   Prevents error when reloading the page after making the POST.
+    redirect '/dictionary'
+  else
+    @current_dictionary = Word.sort
+    @duplicate = word.the_word
+    erb(:dictionary)
+  end
 end
 
 get('/dictionary') do
   @current_dictionary = Word.sort
+  @last_word = Word.time_sift.the_word
+  @duplicate = false
   erb(:dictionary)
 end
 
 get('/:id') do
+  @definitions = Word.find(params[:id]).definition
+  @define = Word.find(params[:id])
+  @empty = false
+  erb(:definition)
+end
 
+post('/:id') do
+  if params["new_definition"] != ""
+    Word.find(params[:id]).add_definition(params["new_definition"])
+    redirect '/'+params[:id]
+  else
+    @definitions = Word.find(params[:id]).definition
+    @define = Word.find(params[:id])
+    @empty = true
+    erb(:definition)
+  end
 end
--- lib/word.rb
+++ lib/word.rb
@@ -20,16 +20,38 @@ class Word
 
   def self.sort()
     @@dictionary.sort_by {|word| word.the_word.downcase}
-
   end
 
   def self.all()
     @@dictionary
   end
 
+  def self.find(id)
+    @@dictionary.each do |word|
+      if word.id == id
+        return word
+      end
+    end
+  end
+
   def save()
+    # built in duplicate word checker
+    @@dictionary.each do |word|
+      if word.the_word.downcase == self.the_word.downcase
+       return false
+     end
+    end
     @@dictionary.push(self)
   end
+
+  def add_definition(a_definition)
+    @definition.push({:definition => "\"" + a_definition + "\"", :timestamp => Time.new})
+  end
+
+  def self.time_sift()
+      temp_dict = @@dictionary.sort_by {|word| @@hashids.decode(word.id)[0]}
+      temp_dict.last
+    end
 
   def decodr()
     Time.at(@@hashids.decode(self.id)[0]).strftime("%m/%d/%Y %l:%M %p")
