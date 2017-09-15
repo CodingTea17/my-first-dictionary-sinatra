fbd39ed93642bc66f32202468e9adaa5bee0609d
Dawson M <dawson.mortenson@gmail.com>
2017-09-15 13:59:17 -0700

    add a patch that corrects the success/warning/danger alerts to show only after being added and not upon loading

--- app.rb
+++ app.rb
@@ -11,8 +11,12 @@ end
 post('/new_word') do
   word = Word.new(params["new_word"])
   if word.save()
+    @last_word = Word.time_sift.the_word
+    @current_dictionary = Word.sort
+    @duplicate = false
+    erb(:dictionary)
     # Super awesome redirect that will reroute the user to a GET route after the new word is saved to the dictionary.   Prevents error when reloading the page after making the POST.
-    redirect '/dictionary'
+    # redirect '/dictionary'
   else
     @current_dictionary = Word.sort
     @duplicate = word.the_word
@@ -22,25 +26,26 @@ end
 
 get('/dictionary') do
   @current_dictionary = Word.sort
-  @last_word = Word.time_sift.the_word
-  @duplicate = false
+  @duplicate = "loader"
   erb(:dictionary)
 end
 
 get('/:id') do
   @definitions = Word.find(params[:id]).definition
   @define = Word.find(params[:id])
-  @empty = false
+  @empty = "loader"
   erb(:definition)
 end
 
 post('/:id') do
+  @definitions = Word.find(params[:id]).definition
+  @define = Word.find(params[:id])
   if params["new_definition"] != ""
     Word.find(params[:id]).add_definition(params["new_definition"])
-    redirect '/'+params[:id]
+    @empty = false
+    erb(:definition)
+    # redirect '/'+params[:id]
   else
-    @definitions = Word.find(params[:id]).definition
-    @define = Word.find(params[:id])
     @empty = true
     erb(:definition)
   end
